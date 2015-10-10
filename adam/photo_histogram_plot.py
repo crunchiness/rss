2af839c0dbe65f97c5367a34677fdf1a4475d939
Adam Golinski <s1229575@giry.inf.ed.ac.uk>
2015-10-10 14:37:16 +0100

    nice histogram script

--- adam/photo_histogram_plot.py
+++ adam/photo_histogram_plot.py
@@ -1,13 +1,12 @@
+import numpy as np
 import cv2
+import rss_adam_utils as utils
+import os
 from matplotlib import pyplot as plt
 
-i = cv2.imread('1.jpg')
+path = '../resources/'
+file = '0.jpg'
 
-i = cv2.cvtColor( i, cv2.COLOR_RGB2HSV)
-
-hue_hist = cv2.calcHist([i], [0], None, [180], [0, 180])
-sat_hist = cv2.calcHist([i], [1], None, [256], [0, 256])
-
-plt.plot(hue_hist)
-plt.plot(sat_hist)
-plt.show()
+for file in os.listdir(path):
+    print(path + file)
+    utils.hsv_hist(path + file)
