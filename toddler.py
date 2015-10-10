6bf873e06f2009b55f4eb9adad4a0378ab9105d0
Ingvaras Merkys <ingvaras@gmail.com>
2015-10-10 23:52:53 +0100

    clean up

--- toddler.py
+++ toddler.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-__TODDLER_VERSION__ = "1.0.0"
+__TODDLER_VERSION__ = '1.0.0'
 
 import time
-import numpy
-import cv2
-import datetime
 
-# Hardware test code
+
 class Toddler:
     def __init__(self, IO):
-        print 'I am a toddler playing in a sandboxz'
+        print '{{some cool name}} has awoken... Beware.'
         self.IO = IO
 
     def move(self, l=100, r=100):
@@ -18,7 +15,7 @@ class Toddler:
     def close_gate(self):
         self.IO.servoEngage()
         self.IO.servoSet(0)
-        time.sleep(2)
+        time.sleep(2)  # seems like we need this for servo?
         self.IO.servoDisengage()
 
     def open_gate(self):
@@ -28,10 +25,8 @@ class Toddler:
         self.IO.servoDisengage()
 
     # This is a callback that will be called repeatedly.
-
     # It has its dedicated thread so you can keep block it.
     def Control(self, OK):
-
         while OK():
             self.open_gate()
             time.sleep(5)
@@ -42,42 +37,3 @@ class Toddler:
     # It has its dedicated thread so you can keep block it.
     def Vision(self, OK):
         pass
-        # self.IO.cameraSetResolution('low')
-
-        # hasImage = False
-        # res = 0
-        # sw = False
-        # swPrev = False
-        # while OK():
-        #     if self.inp[4]:
-        #         for i in range(0, 5):
-        #             self.IO.cameraGrab()
-        #         img = self.IO.cameraRead()
-        #         if img.__class__ == numpy.ndarray:
-        #             hasImage = True
-        #             cv2.imwrite('camera-' + datetime.datetime.now().isoformat() + '.png', img)
-        #             self.IO.imshow('window', img)
-        #             self.IO.setStatus('flash', cnt=2)
-        #             time.sleep(0.5)
-        #     if hasImage:
-        #         self.IO.imshow('window', img)
-        #
-        #     sw = self.inp[5]
-        #     if sw != swPrev and sw:
-        #         res = (res + 1) % 4
-        #         if res == 0:
-        #             self.IO.cameraSetResolution('low')
-        #             self.IO.setError('flash', cnt=1)
-        #         if res == 1:
-        #             self.IO.cameraSetResolution('medium')
-        #             self.IO.setError('flash', cnt=2)
-        #         if res == 2:
-        #             self.IO.cameraSetResolution('high')
-        #             self.IO.setError('flash', cnt=3)
-        #         if res == 3:
-        #             self.IO.cameraSetResolution('full')
-        #             self.IO.setError('flash', cnt=4)
-        #         time.sleep(0.5)
-        #     swPrev = sw
-        #
-        #     time.sleep(0.05)
--- vision/rss_adam_utils.py
+++ vision/rss_adam_utils.py
@@ -2,33 +2,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
+
 def hsv_hist(file):
     hsv_map = np.zeros((180, 256, 3), np.uint8)
     h, s = np.indices(hsv_map.shape[:2])
-    hsv_map[:,:,0] = h
-    hsv_map[:,:,1] = s
-    hsv_map[:,:,2] = 255
+    hsv_map[:, :, 0] = h
+    hsv_map[:, :, 1] = s
+    hsv_map[:, :, 2] = 255
     hsv_map = cv2.cvtColor(hsv_map, cv2.COLOR_HSV2BGR)
 
     bgr = cv2.imread(file)
     hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
-    dark = hsv[...,2] < 32
+    dark = hsv[..., 2] < 32
     hsv[dark] = 0
-    h = cv2.calcHist( [hsv], [0, 1], None, [180, 256], [0, 180, 0, 256] )
+    h = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
 
     hist_scale = 10
-    h1 = np.clip(h*0.005*hist_scale, 0, 1)
-    vis_bgr = hsv_map*h1[:,:,np.newaxis] / 255.0
+    h1 = np.clip(h * 0.005 * hist_scale, 0, 1)
+    vis_bgr = hsv_map * h1[:, :, np.newaxis] / 255.0
 
     fig = plt.figure()
 
-    a = fig.add_subplot(1,2,1)
+    fig.add_subplot(1, 2, 1)
     rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-    imgplot = plt.imshow(rgb)
+    plt.imshow(rgb)
 
-    a = fig.add_subplot(1,2,2)
+    fig.add_subplot(1, 2, 2)
     vis_rgb = cv2.cvtColor(vis_bgr, cv2.COLOR_BGR2RGB)
-    imgplot = plt.imshow(vis_rgb)
+    plt.imshow(vis_rgb)
     plt.xlabel('saturation')
     plt.ylabel('hue')
 
