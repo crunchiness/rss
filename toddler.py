e1a1dc6b5efcaa3d104dc6e624fc87cb3356f098
crunchiness <s1137752@sms.ed.ac.uk>
2015-10-10 23:42:29 +0100

    commands for opening/closing gates; some empty packages

--- toddler.py
+++ toddler.py
@@ -11,57 +11,73 @@ class Toddler:
     def __init__(self, IO):
         print 'I am a toddler playing in a sandboxz'
         self.IO = IO
-        self.inp = [0, 0, 0, 0, 0, 0, 0, 0]
 
     def move(self, l=100, r=100):
         self.IO.setMotors(l, -r)
+
+    def close_gate(self):
+        self.IO.servoEngage()
+        self.IO.servoSet(0)
+        time.sleep(2)
+        self.IO.servoDisengage()
+
+    def open_gate(self):
+        self.IO.servoEngage()
+        self.IO.servoSet(180)
+        time.sleep(2)
+        self.IO.servoDisengage()
 
     # This is a callback that will be called repeatedly.
 
     # It has its dedicated thread so you can keep block it.
     def Control(self, OK):
+
         while OK():
-            self.move(100, -100)
-            time.sleep(0.05)
+            self.open_gate()
+            time.sleep(5)
+            self.close_gate()
+            time.sleep(5)
 
     # This is a callback that will be called repeatedly.
     # It has its dedicated thread so you can keep block it.
     def Vision(self, OK):
-        self.IO.cameraSetResolution('low')
-        hasImage = False
-        res = 0
-        sw = False
-        swPrev = False
-        while OK():
-            if self.inp[4]:
-                for i in range(0, 5):
-                    self.IO.cameraGrab()
-                img = self.IO.cameraRead()
-                if img.__class__ == numpy.ndarray:
-                    hasImage = True
-                    cv2.imwrite('camera-' + datetime.datetime.now().isoformat() + '.png', img)
-                    self.IO.imshow('window', img)
-                    self.IO.setStatus('flash', cnt=2)
-                    time.sleep(0.5)
-            if hasImage:
-                self.IO.imshow('window', img)
+        pass
+        # self.IO.cameraSetResolution('low')
 
-            sw = self.inp[5]
-            if sw != swPrev and sw:
-                res = (res + 1) % 4
-                if res == 0:
-                    self.IO.cameraSetResolution('low')
-                    self.IO.setError('flash', cnt=1)
-                if res == 1:
-                    self.IO.cameraSetResolution('medium')
-                    self.IO.setError('flash', cnt=2)
-                if res == 2:
-                    self.IO.cameraSetResolution('high')
-                    self.IO.setError('flash', cnt=3)
-                if res == 3:
-                    self.IO.cameraSetResolution('full')
-                    self.IO.setError('flash', cnt=4)
-                time.sleep(0.5)
-            swPrev = sw
-
-            time.sleep(0.05)
+        # hasImage = False
+        # res = 0
+        # sw = False
+        # swPrev = False
+        # while OK():
+        #     if self.inp[4]:
+        #         for i in range(0, 5):
+        #             self.IO.cameraGrab()
+        #         img = self.IO.cameraRead()
+        #         if img.__class__ == numpy.ndarray:
+        #             hasImage = True
+        #             cv2.imwrite('camera-' + datetime.datetime.now().isoformat() + '.png', img)
+        #             self.IO.imshow('window', img)
+        #             self.IO.setStatus('flash', cnt=2)
+        #             time.sleep(0.5)
+        #     if hasImage:
+        #         self.IO.imshow('window', img)
+        #
+        #     sw = self.inp[5]
+        #     if sw != swPrev and sw:
+        #         res = (res + 1) % 4
+        #         if res == 0:
+        #             self.IO.cameraSetResolution('low')
+        #             self.IO.setError('flash', cnt=1)
+        #         if res == 1:
+        #             self.IO.cameraSetResolution('medium')
+        #             self.IO.setError('flash', cnt=2)
+        #         if res == 2:
+        #             self.IO.cameraSetResolution('high')
+        #             self.IO.setError('flash', cnt=3)
+        #         if res == 3:
+        #             self.IO.cameraSetResolution('full')
+        #             self.IO.setError('flash', cnt=4)
+        #         time.sleep(0.5)
+        #     swPrev = sw
+        #
+        #     time.sleep(0.05)
