57772c303f201483b3030077564142e304051b3d
talesa <adam.gol1993@gmail.com>
2015-10-11 02:48:07 +0100

    fixed main function for determining boundaries

--- vision/photo_histogram_plot.py
+++ vision/photo_histogram_plot.py
@@ -2,8 +2,7 @@ import rss_adam_utils as utils
 import os
 
 path = '../resources/'
-pic_file = '0.jpg'
 
 for pic_file in os.listdir(path):
-    print(path + pic_file)
-    utils.hsv_hist(path + pic_file)
+    if '.jpg' in pic_file:
+    	utils.determine_boundaries(path + pic_file)
