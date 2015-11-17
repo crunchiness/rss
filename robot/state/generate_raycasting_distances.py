6e49729d57fa3452ac92f4620cdb8600502c723c
Adam Golinski <adam.gol1993@gmail.com>
2015-11-17 02:14:45 +0000

    generate raycasting

--- robot/state/generate_raycasting_distances.py
+++
@@ -1,5 +0,0 @@
-import particle_filtering
-import sys
-
-a = particle_filtering.Particles.generate_raycasting_distances(sys.args[0], sys.args[1])
-particle_filtering.Particles.save_numpy_array('RAYCASTING_DISTANCES_{}_{}'.format(sys.args[0], sys.args[1]), a)
